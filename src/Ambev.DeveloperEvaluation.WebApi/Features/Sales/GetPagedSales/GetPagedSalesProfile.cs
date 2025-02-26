96426e49b3eda810024a72f054b040c4c4bb7aa0
Marcelo Camargos <marcelocamargosjr@gmail.com>
2025-02-26 00:46:15 -0300

    :sparkles: feat: add GetPagedSales to the WebApi layer

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetPagedSales/GetPagedSalesProfile.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.GetPagedSales;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetPagedSales;
+
+/// <summary>
+///     Profile for mapping GetPagedSales feature requests and responses.
+/// </summary>
+public class GetPagedSalesProfile : Profile
+{
+    public GetPagedSalesProfile()
+    {
+        CreateMap<GetPagedSalesRequest, GetPagedSalesQuery>();
+        CreateMap<GetPagedSalesResult, GetPagedSalesResponse>();
+    }
+}
