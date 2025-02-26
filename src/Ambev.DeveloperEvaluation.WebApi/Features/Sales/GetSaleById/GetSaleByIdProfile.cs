f67cf5bd624cf01a0d6674b9f3d14ba262282089
Marcelo Camargos <marcelocamargosjr@gmail.com>
2025-02-26 00:50:19 -0300

    :sparkles: feat: add GetSaleById to the WebApi layer

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSaleById/GetSaleByIdProfile.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.GetSaleById;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSaleById;
+
+/// <summary>
+///     Profile for mapping GetSaleById feature requests and responses.
+/// </summary>
+public class GetSaleByIdProfile : Profile
+{
+    public GetSaleByIdProfile()
+    {
+        CreateMap<GetSaleByIdRequest, GetSaleByIdQuery>();
+        CreateMap<GetSaleByIdResult, GetSaleByIdResponse>();
+    }
+}
